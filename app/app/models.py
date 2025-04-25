be4205b94ce60faacbc5044ece33ae368852b4c5
AbdelazizKafi <2024_abdelaziz.kafi@iticbcn.cat>
2025-04-25 19:12:11 +0200

    Modelos provisionales

---
+++ app/app/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.utils import timezone
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    creation_date = models.DateTimeField(default=timezone.now)
+    bio = models.CharField(max_length=200)
+    followers = models.ManyToManyField(blank=True)
+
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(upload_to='posts/')
+    description = models.TextField()
+    num_likes = models.IntegerField(default=0)
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.creator.username} on {self.date_creation.date()}"
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self):
+        return f"{self.user.username} liked Post {self.post.id}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} commented on Post {self.post.id}"
